b67c86b663ebfa06dfe38445eaeb2b1688aa8649
SaiTej6860 <40879163+SaiTej6860@users.noreply.github.com>
2018-07-06 07:06:39 -0700

    Update singleton.rb

--- singleton.rb
+++ singleton.rb
@@ -1,1 +1,20 @@
-# work
+class sample
+end
+s=sample.new
+class<<s
+def manager (name, payrate)
+@n=name
+@p=payrate
+end
+def employee (name, payrate)
+@n=name
+@p=payrate
+end
+def pay(hours)
+@p*=hours
+puts "The employee #{@n} pay of salary is #{@p}"
+end
+s.employee ("Sai", 120)
+  s.pay (8)
+  s.manager ("Tej", 300)
+  s.pay (4)
